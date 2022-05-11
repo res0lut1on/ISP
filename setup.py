f32228ba45f33bae11521a62cb5cf76632f03339
John Wayne <navimuxacek@mail.ru>
2022-05-12 00:04:34 +0300

    add toml

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Good Programs For Your PC',
+    version='1.0',
+    description='no matter how, no matter where, the main thing is together and the main thing is not in the',
+    author='ya',
+    author_email='navimuxacek@mail.ru',
+    url='https://www.dotabuff.com/players/70388657',
+    packages=['serial_modules', 'serializers'],
+    scripts=['bin/serial']
+)
