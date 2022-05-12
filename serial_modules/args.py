0dfcb6cfd00baa55dd804b70cffc9441f5818363
John Wayne <navimuxacek@mail.ru>
2022-05-12 13:17:43 +0300

    add parser

--- serial_modules/args.py
+++ serial_modules/args.py
@@ -1,18 +1,38 @@
 import argparse
 
-class ArgParcer:
+class ArgParser:
     """Varbl from cmd line arg"""
-    parcer = argparse.ArgumentParser(description="Great Description To Be Here")
+    @staticmethod
+    def get_parser():
+        parser = argparse.ArgumentParser(description="Great Description To Be Here")
 
-    parcer.add_argument('l', '--load',
-                        dest='load', nargs='+',
-                        metavar='filename',
-                        help='load obj from files')
-    parcer.add_argument('d', '--dump',
-                        dest='dump', nargs='+',
-                        metavar='filename:object:filetype',
-                        help='dump obj from files')
-    parcer.add_argument('l', '--load',
-                        dest='load', nargs='+',
-                        metavar='filename',
-                        help='load obj from files')
+        parser.add_argument('l', '--load',
+                            dest='load', nargs='+',
+                            metavar='filename',
+                            help='load obj from files')
+        parser.add_argument('d', '--dump',
+                            dest='dump', nargs='+',
+                            metavar='filename:object:filetype',
+                            help='dump obj from files')
+        parser.add_argument('c', '--convert',
+                            dest='convert', nargs='+',
+                            metavar='filename filetype',
+                            help='convert')
+        return parser
+
+    @staticmethod
+    def parse():
+        parser = ArgParser.get_parser()
+        return parser.parse_args()
+
+    @staticmethod
+    def get_args():
+        """Return command args."""
+        args = ArgParser.parse()
+        return (args.dump, args.load, args.convert)
+
+    @staticmethod
+    def print_help():
+        """Print cmd help"""
+        parser = ArgParser.get_parser()
+        parser.print_help()
